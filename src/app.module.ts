c4e5b28cfaa05f6d327ab4edf9f14fa18c6b859c
Branco Sanchez <bsanchez@acl.cl>
2024-11-14 13:39:19 -0300

    Add @nestjs/config dependency and ignore package-lock.json
    Include @nestjs/config to manage configuration settings within the project. Update .gitignore to exclude package-lock.json to avoid potential conflicts in team environments.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables de configuración estén disponibles globalmente
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true, // Carga automáticamente las entidades
+        synchronize: true, // ¡Solo para desarrollo!
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
