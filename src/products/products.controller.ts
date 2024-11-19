898904e52a20bdd759ade51612292ca2c7c5fc3d
Luis Serrano Miranda <83100833+luisserranom@users.noreply.github.com>
2024-11-19 17:04:07 -0300

    se agregan mejoras de metodos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller,Body,Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload, RpcException } from '@nestjs/microservices';
+
+
 
 @Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-
+  
   
   @MessagePattern("createProduct")
   create(@Body() createProductDto: CreateProductDto) {
@@ -20,19 +22,22 @@ export class ProductsController {
   }
 
   @MessagePattern("findOneProduct")
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  async findOne(@Body() id: number) {
+    console.log('Received ID:', id); // Depura el ID recibido
+    return this.productsService.findOne(id); // Asegúrate de que sea un número
   }
 
   @MessagePattern("updateProduct")
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(@Body() data: { id: number, updateProductDto: UpdateProductDto }) {
+    return this.productsService.update(data.id, data.updateProductDto);
   }
 
   @MessagePattern("deleteProduct")
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  async remove(@Payload() id: number): Promise<string> {
+    await this.productsService.remove(id);
+    return `Product with ID ${id} deleted successfully`;
   }
+
 
  
   @MessagePattern("findByName")
